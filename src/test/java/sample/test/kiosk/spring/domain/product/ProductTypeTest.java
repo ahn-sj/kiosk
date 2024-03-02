313534d90326ff8f39d13d2950c0243ab09532ec
ahn-sj <jaesa5221@gmail.com>
2024-03-02 22:00:42 +0900

    feat: 섹션 7-7. ParameterizedTest

--- src/test/java/sample/test/kiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/test/kiosk/spring/domain/product/ProductTypeTest.java
@@ -2,6 +2,8 @@ package sample.test.kiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,6 +36,22 @@ class ProductTypeTest {
 
         // then
         assertThat(result).isTrue();
+
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @ParameterizedTest(name = "{index} ==> 상품 타입: {0}, 재고 관련 타입 여부: {1}")
+    @CsvSource({
+            "HANDMADE, false",
+            "BAKERY, true"
+    })
+    void containsStockType4(ProductType givenType, boolean expected) {
+
+            // when
+            boolean result = ProductType.containsStockType(givenType);
+
+            // then
+            assertThat(result).isEqualTo(expected);
 
     }
 }
